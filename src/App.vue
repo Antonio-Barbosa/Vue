2003eeee6785eda7ef09a8111b9441ee0fda5b8d
Antonio Marcelo <antoniomarcelo545@gmail.com>
2024-03-11 13:20:19 +0000

    Aprendendo sobre Vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Teste Vue!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -16,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
+  <!-- Onde eu quiser usar meu conteúdo da página, coloco RouterView -->
   <RouterView />
 </template>
 
